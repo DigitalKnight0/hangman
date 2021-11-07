2e4637f7fe6a543a37733bf6b0b5843b40a5eb0b
DigitalKnight <asadredset@gmail.com>
2021-11-07 22:41:59 +0500

    working

---
+++ lib/wordgen.rb
@@ -0,0 +1,25 @@
+module Wordgen
+
+    def get_list
+        sample = File.open("../5desk.txt" , "r")
+        words = sample.readlines
+        sample.close 
+        words  
+    end
+
+    def gen_word
+      myWord = ""
+      loop do
+        list = get_list
+        random_id = rand(0..list.size)
+        myWord = list[random_id]
+        if myWord.length > 3
+            
+            break
+        end
+      end
+      myWord
+    end
+end
+
+
